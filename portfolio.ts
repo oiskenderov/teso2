d6c545564611d21b90a57c764dcd7e7385c0b33c
orkhanelite <oiskenderov@gmail.com>
2023-06-09 23:37:53 +0400

    changes in portfolio

--- portfolio.ts
+++ portfolio.ts
@@ -12,16 +12,16 @@ import {
 } from './types/sections';
 
 export const greetings: GreetingsType = {
-  name: 'Hanzla Tauqeer',
-  title: "Hi all, I'm Hanzla",
+  name: 'Orkhan Iskenderov',
+  title: "Hi all, I'm Orkhan",
   description:
-    "I'm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Next.js and Blockchain development on Ethereum, Solidity, Web3.js, Moralis, and Brownie Framework.",
+    "I'm passionate DevOps Engineer and having an experience Docker, Python, Bash, Kubernetes, Git, AWS, Ansible, Terraform.",
   resumeLink:
-    'https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing',
+    'https://docs.google.com/document/d/1sP2Wu7WbzAwoZZbAwyjYz-kd_ubnLA41/edit?usp=sharing&ouid=109134442709565969366&rtpof=true&sd=true',
 };
 
 export const openSource = {
-  githubUserName: '1hanzla100',
+  githubUserName: 'oiskenderov',
 };
 
 export const contact = {};
